1473112bbbae928a8e414ee903df637474449f42
Pedro Tatibano <pedrotatibano190@gmail.com>
2025-03-18 00:48:20 -0300

    feat: enable CORS for the application to allow requests from specified origin

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001' // ou use "*" para todas as origens, se apropriado
+  });
+  
   app.useGlobalFilters(
     new HttpExceptionFilter(),
     new ValidationExceptionFilter(),
