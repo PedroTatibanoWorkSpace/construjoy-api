6b975b6f132ebe86559c13878f15ad1e768a498e
Pedro Tatibano <pedrotatibano190@gmail.com>
2025-03-27 19:15:54 -0300

    feat: add purchase date field to receivable entity update logic

--- src/modules/receivables/domain/entities/receivable.entity.ts
+++ src/modules/receivables/domain/entities/receivable.entity.ts
@@ -41,6 +41,7 @@ export class Receivable {
     this.clientId = updateData.clientId ?? this.clientId;
     this.value = updateData.value ?? this.value;
     this.description = updateData.description ?? this.description;
+    this.purchaseDate = updateData.purchaseDate ?? this.purchaseDate;
     this.validate = updateData.validate ?? this.validate;
     this.paymentDate = updateData.paymentDate ?? this.paymentDate;
     this.paymentStatus = updateData.paymentStatus ?? this.paymentStatus;
